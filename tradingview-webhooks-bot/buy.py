a9c9befff4b9acafad26fd471d2a79e86ec44900
avatar-lavventura <alper.alimoglu@gmail.com>
2021-05-22 19:59:44 +0300

    Performance from 03.01 => 731% ALL | 518% Long , 213% Short

--- tradingview-webhooks-bot/buy.py
+++ tradingview-webhooks-bot/buy.py
@@ -6,31 +6,34 @@
 
 import time
 from pathlib import Path
+
+from tools import log
 from user_setup import check_binance_obj
-from utils import log
 
 HOME = str(Path.home())
 AMOUNT = {}
-AMOUNT["DOGEUSDT"] = 1240
-# AMOUNT = 8000  # 1000Shibi
-# TODO: 300$ lik al yap
+AMOUNT["DOGEUSDT"] = 100
 
 
 class Strategy:
     def __init__(self, data_msg):
-        print(data_msg)
-        # {{ticker}},{{strategy.order.action}},{{strategy.market_position}},{{strategy.prev_market_position}},{{timenow}}
-        self.chunks = data_msg.split(",")
-        self.symbol = self.chunks[0].replace("PERP", "")
-        self.side = self.chunks[1]
-        self.market_position = self.chunks[2]
-        self.prev_market_position = self.chunks[3]
-        self.timenow = self.chunks[4]
-        log(f"==> symbol={self.symbol}")
-        log(f"==> side={self.side}")
-        log(f"==> market_position={self.market_position}")
-        log(f"==> prev_market_position={self.prev_market_position}")
-        log(f"==> timenow={self.timenow}")
+        log(data_msg, color="green")
+        try:
+            self.chunks = data_msg.split(",")
+            self.symbol = self.chunks[0].replace("PERP", "")
+            self.side = self.chunks[1]
+            self.market_position = self.chunks[2]
+            self.prev_market_position = self.chunks[3]
+            self.timenow = self.chunks[4]
+            self.position_size = self.chunks[5]
+            log(f"==> symbol={self.symbol}")
+            log(f"==> side={self.side}")
+            log(f"==> market_position={self.market_position}")
+            log(f"==> prev_market_position={self.prev_market_position}")
+            log(f"==> timenow={self.timenow}")
+            log(f"==> position_size={self.position_size}")
+        except:
+            pass
 
 
 class BotHelper:
@@ -48,49 +51,59 @@ class BotHelper:
                 symbol=symbol, side="BUY", type="MARKET", quantity=amount, reduceOnly=True
             )
 
-        print(f"{order}\n")
+        log(f"{order}\n")
 
     def sell(self, symbol, amount):
         amount = abs(float(amount))
-        print(f"amount={amount}")
+        log(f"amount={amount}")
         try:
             order = self.client.futures_create_order(symbol=symbol, side="SELL", type="MARKET", quantity=amount)
-            print(f"{order}\n")
+            log(f"{order}\n")
         except:
             try:
                 order = self.client.futures_create_order(
                     symbol=symbol, side="SELL", type="MARKET", quantity=amount, reduceOnly=True
                 )
-                print(f"{order}\n")
+                log(f"{order}\n")
             except Exception as e:
-                print(str(e))  # ReduceOnly Order is rejected or // Margin is insufficient.
+                log(str(e), color="red")  # ReduceOnly Order is rejected or // Margin is insufficient.
+
+    def get_open_position_side(self, _symbol) -> bool:
+        futures = self.client.futures_position_information(symbol=_symbol)
+        for future in futures:
+            amount = future["positionAmt"]
+            if amount != "0":  # if there is position
+                if future["entryPrice"] > future["liquidationPrice"]:
+                    return "long"
+                else:
+                    return "short"
 
     def strategy_exit(self, strategy):
         futures = self.client.futures_position_information(symbol=strategy.symbol)
         for future in futures:
             amount = future["positionAmt"]
             if amount != "0":  # if there is position
-                open_position_side = None
-                if future["entryPrice"] > future["liquidationPrice"]:
-                    open_position_side = "long"
-                else:
-                    open_position_side = "short"
-
-                print(f"==> {open_position_side}_x{future['leverage']}")
+                current_position_side = self.get_open_position_side(strategy.symbol)
+                log(f"==> {current_position_side}_x{future['leverage']}")
+                log("")
                 if strategy.market_position == "flat":
-                    log(f"==> CLOSING {open_position_side} position")
-                    if open_position_side == "long":
+                    log(f"==> CLOSING {current_position_side} position")
+                    if current_position_side == "long":
                         self.sell(strategy.symbol, amount)
                     else:
                         self.buy(strategy.symbol, amount)
-                elif strategy.market_position != open_position_side:
-                    log(f"==> CLOSING {open_position_side} position")
+                elif strategy.market_position != current_position_side:
+                    log(f"==> CLOSING {current_position_side} position")
                     if strategy.market_position == "short":
                         self.sell(strategy.symbol, amount)
                     else:
                         self.buy(strategy.symbol, amount)
                 else:
-                    log("Warning: already opened position side and requested position side is same. Do nothing.")
+                    log(
+                        "Warning: already opened position, where on going position side and requested position side are"
+                        " the same. Do nothing."
+                    )
+                    log(future)
                     return False
         return True
 
--- tradingview-webhooks-bot/run_app.sh
+++ tradingview-webhooks-bot/run_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-nohup ./app.py > app.log 2>&1 &
-tail -f app.log
+LOG_FILE=/home/alper_alimoglu_research2/.eBlocBroker/provider.log
+nohup python3 -u app.py >> $LOG_FILE 2>&1 &
+sleep 1
+tail -f $LOG_FILE
