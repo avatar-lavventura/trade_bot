a3a15e2156132e4e0bd25d356eeda17fa667ce91
avatar-lavventura <alper.alimoglu@gmail.com>
2022-10-29 16:08:42 +0300

    Track multiple assets in the menubar

--- bot/menubar/tracker.py
+++ bot/menubar/tracker.py
@@ -1,47 +1,66 @@
 #!/usr/bin/env python3
 
-import time
-import ccxt  # noqa: E402
-import rumps
-
-# rumps.debug_mode(True)
 # __ https://github.com/jaredks/rumps
 # __ https://github.com/srid/org-clock-dashboard
 
+import time
+import ccxt
+import rumps
+import subprocess
+
+# rumps.debug_mode(True)
 exchange = ccxt.binance({"options": {"adustForTimeDifference": True}, "enableRateLimit": True})
+assets = ["DOGEUSDT", "DOGEBTC", "ORNBTC"]
+timer_duration = 2
+# sold = "ORNBTC"
+# bought = "DOGEBTC"
+# amount = {}
+# amount["ORNBTC"] = 735.6
+# amount["DOGEBTC"] = 7787
 
 
 def run(cmd):
-    import subprocess
-
     return subprocess.check_output(cmd, shell=True).strip()
 
 
 def tracker_clock_string():
-    output = exchange.fetch_ticker("ORNBTC")
-    return "{:.8f}".format(output["last"]).strip("0")[1:].strip("0")
+    msg = ""
+    for asset in reversed(assets):
+        output = exchange.fetch_ticker(asset)
+        price_last = output["last"]
+        price = "{:.8f}".format(price_last).strip("0")[1:].strip("0")
+        if not msg:
+            msg = f"{asset} {price}"
+        else:
+            msg = f"{asset} {price} | {msg}"
+
+    return msg
 
 
 class OrgClockStatusBarApp(rumps.App):
     @rumps.clicked("Refresh")
-    def sayhi(self, _):
+    def update_ticker(self, _):
         clock_title = tracker_clock_string()
         self.title = clock_title
 
 
-if __name__ == "__main__":
-    app = OrgClockStatusBarApp("alpy")
+def main():
+    app = OrgClockStatusBarApp("starting...")
 
-    def timer_func(sender):  # noqa
-        s = tracker_clock_string()
-        print(s)  # removing the print statement makes the app hang
-        if s is not None:
-            app.title = s
+    def timer_func(timer_duration):
+        _str = tracker_clock_string()
+        print(_str)  # removing the print statement makes the app hang
+        if _str:
+            app.title = _str
         else:
             app.title = "Not tracking"
 
-        time.sleep(10)
+        time.sleep(timer_duration)
 
-    timer = rumps.Timer(timer_func, 5)
+    timer = rumps.Timer(timer_func, timer_duration)
     timer.start()
     app.run()
+
+
+if __name__ == "__main__":
+    main()
